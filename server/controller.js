28e742ddf101888c9860d08fe36daf737af06a98
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-10-12 00:26:28 -0600

    second commit

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,17 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+
+      
     }
 
+    // getFortune: (req, res) => {
+    //     const fortunes = ["A faithful friend is a strong defense.", "Any decision you have to make tomorrow is a good decision.",
+    //      "Believe it can be done.", "Living with a commitment to excellence shall take you far.", "Self-knowledge is a life long process."]
+    //     let randomIndex = Math.floor(Math.random() * fortunes.length);
+    //     let randomFortune = fortunes[randomIndex];
+         
+    //      res.status(200).send(randomFortune)
+    // }
 }
+
--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.use(express.json());
+
+
+
 
 const { getCompliment } = require('./controller')
+// const { getFortune } = require('./controller')
 
-app.get("/api/compliment", getCompliment);
+
+
+app.get("/api/compliment", getCompliment); 
+// app.get("/api/fortune", getFortune);
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
